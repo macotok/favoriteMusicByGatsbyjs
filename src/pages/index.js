48b10a20928520c1ef3c3657b40bead13be8c083
macotok <monochromer@r8.ucom.ne.jp>
2019-12-09 00:08:14 +0900

    TOPページのテキストをmapで展開

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
+import { Link } from 'gatsby';
+import topText from '../text/top';
 
 const Home = () => (
   <Layout>
-
+    <main>
+      <p>
+        ぷこおの勝手な音楽評論のサイトです。<br />
+        これまでどういう音楽を聴いてきたか。どういう音楽が好きかを綴るサイトです。
+      </p>
+      {
+        topText.map(list => (
+          <div>
+            <h2>{list.title}</h2>
+            <p>{list.content}</p>
+            <Link to={list.link}>to Page</Link>
+          </div>
+        ))
+      }
+      <p>
+        このサイトはHeadlessCMS(WordPress)とGatsby.js(React)で制作しています。
+      </p>
+    </main>
   </Layout>
 );
 
---
+++ src/text/top.js
@@ -0,0 +1,14 @@
+const topText = [
+  {
+    title: '音楽遍歴',
+    content: 'いままでどういう音楽を聴いてきたかを時系列で綴ってみました。',
+    link: '/history',
+  },
+  {
+    title: '好きな音楽',
+    content: 'いままで聴いてきた好きな音楽をおすすめ付きで紹介します。',
+    link: '/blog',
+  }
+];
+
+export default topText;
