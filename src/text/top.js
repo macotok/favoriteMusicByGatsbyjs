8bf43905189722d5cf923a310afa8a2246e5fa89
macotok <monochromer@r8.ucom.ne.jp>
2019-12-22 02:14:17 +0900

    Topページにcontactのblockを追加

--- src/text/top.js
+++ src/text/top.js
@@ -10,6 +10,12 @@ const topText = [
     content: 'いままで聴いてきた好きな音楽をお気に入り付きで紹介します。',
     link: '/favorite',
     linkText: 'Favorite',
+  },
+  {
+    title: 'お問い合わせ',
+    content: '取り上げて欲しいテーマやミュージシャン、記事に関するお問い合わせを受け付けてます。',
+    link: '/contact',
+    linkText: 'Contact',
   }
 ];
 
